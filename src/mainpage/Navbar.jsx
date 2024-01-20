03b87a751f73eb0a57d935bc78aede32ade54295
Prem Kumar <prem68265@gmail.com>
2024-01-20 20:04:08 +0530

    responsibility added to the header

--- src/mainpage/Navbar.jsx
+++ src/mainpage/Navbar.jsx
@@ -1,6 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
-import './Navbar.css'
+import "./Navbar.css";
 export default function Navbar() {
+  function showoption() {
+    document.getElementById("option-container").style.marginLeft = "0";
+    console.log("hello");
+  }
+  //------------- dismiss funtion-----------
+  function dismiss() {
+    document.getElementById("option-container").style.marginLeft = "-200px";
+  }
   return (
     <>
       <nav>
@@ -8,7 +16,12 @@ export default function Navbar() {
         <div className="nav1">
           <ul className="links-container">
             <li>
-                <img className="round-img" src="src\assets\images\logo.png" alt="logo" height='60'  />
+              <img
+                className="round-img"
+                src="src\assets\images\logo.png"
+                alt="logo"
+                height="60"
+              />
             </li>
             <li>
               <Link to="/">Home</Link>
@@ -27,7 +40,12 @@ export default function Navbar() {
             </li>
             <li>
               <div className="account">
-                    <img src="src\assets\images\account.png" alt="account" className="round-img" height='60' />
+                <img
+                  src="src\assets\images\account.png"
+                  alt="account"
+                  className="round-img"
+                  height="60"
+                />
               </div>
             </li>
           </ul>
@@ -35,11 +53,76 @@ export default function Navbar() {
 
         {/* mobile navbar */}
         <div className="nav2">
-          <div className="menu-icon"></div>
-          <div className="logo"></div>
-          <div className="account"></div>
+          {/* -----------------------menu icons*----------------------*/}
+          <div className="menu-icon" onClick={showoption}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="24"
+              fill="white"
+              class="bi bi-list"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+              />
+            </svg>
+          </div>
+          {/* onlclik display options */}
+
+          {/*---------------------- mobile logo---------------------- */}
+          <div className="logo">
+            <img
+              className="round-img"
+              src="src\assets\images\logo.png"
+              alt="logo"
+              height="60"
+            />
+          </div>
+          {/* -----------------------account ------------------------------- */}
+          <div className="account">
+            <img
+              src="src\assets\images\account.png"
+              alt="account"
+              className="round-img account2"
+              height="60"
+            />
+          </div>
         </div>
       </nav>
+      <div className="option-body" id="option-container">
+        <div id="option-dismiss" onClick={dismiss}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            class="bi bi-arrow-bar-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5"
+            />
+          </svg>
+        </div>
+        {/* Menu links */}
+        <ul>
+          <li className="linkstyle">
+            <Link to="/html" onClick={dismiss}>HTML</Link>
+          </li>
+          <li className="linkstyle">
+            <Link to="/css" onClick={dismiss}>CSS</Link>
+          </li>
+          <li className="linkstyle">
+            <Link to="/java" onClick={dismiss}>JAVA</Link>
+          </li>
+          <li className="linkstyle">
+            <Link to="/react" onClick={dismiss}>REACT</Link>
+          </li>
+        </ul>
+      </div>
       <Outlet />
     </>
   );
